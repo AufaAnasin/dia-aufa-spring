9c12ec5e034a0c92fcbd670e779eaaf61137375a
HelloCaptainAufa737 <42775351+AufaAnasin@users.noreply.github.com>
2023-12-27 11:51:54 +0700

    [TASK 1] AddJob Still Error

--- diafirstday/src/MainClass.java
+++ diafirstday/src/MainClass.java
@@ -17,10 +17,15 @@ public class MainClass {
             int inputCommand = scanner.nextInt();
             if (inputCommand == 1) {
                 System.out.println("Please input new job using following format : [job_id], [job_address], [job_position_id], [job_position_name]");
+                //for multiline input
                 String jobInput = scanner.nextLine();
                 // make the inputted value in the array
                 String[] jobDetails = jobInput.split(",");
-//                Job job = new Job();
+
+                    System.out.println("Test");
+                    Position position = new Position(Integer.parseInt(jobDetails[2]), jobDetails[3]);
+                    Job job = new Job(Integer.parseInt(jobDetails[0]), jobDetails[1], position);
+                    jobController.addJob(job);
             } else if (inputCommand == 2) {
                 jobController.printAllJobs();
             } else if (inputCommand == 3) {
